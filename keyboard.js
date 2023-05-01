65fd2b9e1ca700e4d2474f123532558f0b5dba98
unknown <ivanyuk.cergey@mail.ru>
2023-05-02 02:30:57 +0300

    feat: add elements to html and  keys for keyboard using js

--- keyboard.js
+++ keyboard.js
@@ -1,8 +1,20 @@
+// Create tablearea for text
+let textarea = document.createElement('textarea');
+textarea.className = "textarea use-keyboard-input";
+document.body.append(textarea);
+document.querySelector('textarea').focus()
+
+
+//Create keyboard
 const Keyboard = {
   elements: {
     main: null,
     keysContainer: null,
     keys: []
+  },
+
+  eventHandlers: {
+    input: null
   },
 
   properties: {
@@ -18,23 +30,128 @@ const Keyboard = {
     // Setup main elements
     this.elements.main.classList.add("keyboard");
     this.elements.keysContainer.classList.add("keyboard__keys");
+    this.elements.keysContainer.appendChild(this._createKeys());
+
+    this.elements.keys = this.elements.keysContainer.querySelectorAll(".keyboard__key");
 
     // Add to DOM
     this.elements.main.appendChild(this.elements.keysContainer);
     document.body.appendChild(this.elements.main);
+
+    // Automatically use keyboard for elements with .use-keyboard-input
+    document.querySelectorAll(".use-keyboard-input").forEach(element => {
+      this.inputKey(ddd => {
+        element.value = ddd;
+      });
+    });
   },
 
   _createKeys() {
+    const fragment = document.createDocumentFragment();
+    const keyLayout = [
+      "`", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "Backspace",
+      "Tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]", "\\", "DEL",
+      "Caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "ENTER",
+      "ShiftL", "z", "x", "c", "v", "b", "n", "m", ",", ".", "/", "Up", "ShiftR",
+      "Ctrl", "Win", "Alt", "Space", "Alt", "Ctrl", "Left", "Down", "Right"
+    ];
 
+    keyLayout.forEach(key => {
+      const keyElement = document.createElement("button");
+      const insertLineBreak = ["Backspace", "DEL", "ENTER", "ShiftR"].indexOf(key) !== -1;
+
+      // Add attributes/classes
+      keyElement.setAttribute("type", "button");
+      keyElement.classList.add("keyboard__key");
+
+      // Defining functions for keys
+      switch (key) {
+
+        case "Backspace":
+          keyElement.classList.add("keyboard__key--wide");
+          keyElement.textContent = key;
+
+          keyElement.addEventListener("click", () => {
+            this.properties.value = this.properties.value.substring(0, this.properties.value.length - 1);
+            this._triggerEvent("input");
+          });
+
+          break;
+
+        case "Caps":
+          keyElement.classList.add("keyboard__key--wide", "keyboard__key--activatable");
+          keyElement.textContent = key;
+
+          keyElement.addEventListener("click", () => {
+            this._toggleCapsLock();
+            keyElement.classList.toggle("keyboard__key--active", this.properties.capsLock);
+          });
+
+          break;
+
+        case "ENTER":
+          keyElement.classList.add("keyboard__key--wide");
+          keyElement.textContent = key;
+
+          keyElement.addEventListener("click", () => {
+            this.properties.value += "\n";
+            this._triggerEvent("input");
+          });
+
+          break;
+
+        case "Space":
+          keyElement.classList.add("keyboard__key--space");
+          keyElement.textContent = key;
+
+          keyElement.addEventListener("click", () => {
+            this.properties.value += " ";
+            this._triggerEvent("input");
+          });
+
+          break;
+
+        default:
+          keyElement.textContent = key;
+
+          keyElement.addEventListener("click", () => {
+            this.properties.value += this.properties.capsLock ? key.toUpperCase() : key.toLowerCase();
+            this._triggerEvent("input");
+          });
+
+          break;
+      }
+
+      fragment.appendChild(keyElement);
+
+      if (insertLineBreak) {
+        fragment.appendChild(document.createElement("br"));
+      }
+    });
+
+    return fragment;
   },
 
   _triggerEvent(handlerName) {
-    console.log("Event Triggered! Event Name: " + handlerName);
+    if (typeof this.eventHandlers[handlerName] == "function") {
+      this.eventHandlers[handlerName](this.properties.value);
+    }
   },
 
+  // Changing the state of the Caps Lock key
   _toggleCapsLock() {
-    console.log("CAPS!");
-  }
+    this.properties.capsLock = !this.properties.capsLock;
+
+    for (const key of this.elements.keys) {
+      if (key.className === "keyboard__key") {
+        key.textContent = this.properties.capsLock ? key.textContent.toUpperCase() : key.textContent.toLowerCase();
+      }
+    }
+  },
+
+  inputKey(input) {
+    this.eventHandlers.input = input;
+  },
 };
 
 window.addEventListener("DOMContentLoaded", function () {
